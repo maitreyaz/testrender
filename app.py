8694b21e44def3369b778845fb5520b3edcb6042
Maitreya Zalte <maitreyazalte@gmail.com>
2023-07-15 16:46:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+# if __name__ == '__main__':
+#     app.run()
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +18,5 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    from werkzeug.serving import run_simple
+    run_simple('0.0.0.0', 8080, app)
